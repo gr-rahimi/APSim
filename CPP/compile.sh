531a3a0cdb03e4f536de6549f5199199931ca61d
Gholamreza Rahimi <gholamrezarahimi@d-172-25-98-72.dhcp.virginia.edu>
2018-08-31 08:57:54 -0400

    editing compile.sh for mac

--- CPP/compile.sh
+++ CPP/compile.sh
@@ -3,5 +3,6 @@ rm *.so *.o *.cxx *.pyc *.py
 touch __init__.py
 g++ -c -fPIC -fPIC -std=c++11  VASim.cpp
 swig -c++ -python VASim.i
-g++ -c -fPIC -std=c++11 VASim_wrap.cxx  -I /home/reza/anaconda2/include/python2.7  -I /home/reza/anaconda2/lib/python2.7/
-g++ -shared -Wl,-soname,_VASim.so -o _VASim.so VASim.o VASim_wrap.o
+g++ -c -fPIC -std=c++11 VASim_wrap.cxx  -I ~/anaconda2/include/python2.7
+#g++ -shared -Wl,-soname,_VASim.so -o _VASim.so VASim.o VASim_wrap.o
+g++ -undefined dynamic_lookup  VASim.o VASim_wrap.o -o _VASim.so
