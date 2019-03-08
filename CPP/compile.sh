b0a3d49ecc91814ba1e317366a7dbcdff83fcb7d
Reza Rahimi <rahimi.r.g@gmail.com>
2019-03-08 11:22:04 -0500

    wrong setting

--- CPP/compile.sh
+++ CPP/compile.sh
@@ -4,5 +4,5 @@ touch __init__.py
 g++ -c -fPIC -fPIC -std=c++11  VASim.cpp
 swig -c++ -python VASim.i
 g++ -c -fPIC -std=c++11 VASim_wrap.cxx  -I ~/anaconda2/include/python2.7
-#g++ -shared -Wl,-soname,_VASim.so -o _VASim.so VASim.o VASim_wrap.o
-g++ -undefined dynamic_lookup  VASim.o VASim_wrap.o -o _VASim.so
+g++ -shared -Wl,-soname,_VASim.so -o _VASim.so VASim.o VASim_wrap.o
+#g++ -undefined dynamic_lookup  VASim.o VASim_wrap.o -o _VASim.so
